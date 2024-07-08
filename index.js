3426983b6a8d870d4f88baf70d1817d0e856dbb2
RaneshKonni <konniranesh03@gmail.com>
2024-07-08 10:08:25 +0530

    updates

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const {ApolloServer,gql} = require('apollo-server-express');
+const typeDefs = require('./Schema');
+const cors = require('cors'); // Import CORS
+const resolvers = require('./resolvers');
+const userApiFromRouter = require('./routes/usersRoute')
+const app = express();
+const port = 3001
+const url= 'mongodb+srv://konniranesh03:QFDqUVX2Hqzmy1ET@cluster0.eykf9o0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+// CORS middleware
+app.use(cors());
+app.use(express.json())//parsing
+mongoose.connect(url,{useNewUrlParser:true,
+useUnifiedTopology:true}).then(()=>{console.log('DB connected')})
+.catch((err)=>{console.log(err)});
+const server = new ApolloServer({typeDefs,resolvers});
+app.use('/users',userApiFromRouter); //api
+async function StartServer(){
+ await server.start();
+ server.applyMiddleware({app});//run my express code
+ app.listen(port,()=>{
+ console.log('server live 3001');
+ })
+}
+StartServer();
